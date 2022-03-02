098f099ac57adc61ac1d7b527603b3cd0399ec6c
Dmitriy <dspv@me.com>
2022-03-02 12:25:19 +0200

    Remove duplications

--- variables.tf
+++ variables.tf
@@ -6,7 +6,6 @@ variable name_prefix {}
 variable domain_name {}
 variable aws_lb_arn {}
 variable ecr_region {}
-variable logdna_key {}
 variable ecr_account_id {}
 variable security_groups {}
 variable aws_lb_certificate_arn {}
