6715ab9b3b1692970d22fe08fabfc380debf81b9
Dmitriy <dspv@me.com>
2022-03-30 20:02:31 +0300

    Remove LogDNA and Datadog

--- datadog.tf
+++ datadog.tf
@@ -1,9 +1,9 @@
-# ---------------------------------------------------
-#    Pushing logs from CloudWatch to Datadog
-# ---------------------------------------------------
-resource aws_cloudwatch_log_subscription_filter datadog_log_subscription_filter {
-    name            = "${var.name_prefix}-${var.wm_instance}-${var.service_name}-Datadog-Filter"
-    log_group_name  = "${var.name_prefix}/ecs/${var.cluster_name}/${var.service_name}/"
-    destination_arn = var.datadog_forwarder_arn
-    filter_pattern  = ""
-}
+# # ---------------------------------------------------
+# #    Pushing logs from CloudWatch to Datadog
+# # ---------------------------------------------------
+# resource aws_cloudwatch_log_subscription_filter datadog_log_subscription_filter {
+#     name            = "${var.name_prefix}-${var.wm_instance}-${var.service_name}-Datadog-Filter"
+#     log_group_name  = "${var.name_prefix}/ecs/${var.cluster_name}/${var.service_name}/"
+#     destination_arn = var.datadog_forwarder_arn
+#     filter_pattern  = ""
+# }
