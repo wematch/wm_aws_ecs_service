1e5f5cc48c5f887b38ca84e5e17e715a995571ca
Dmitriy <dspv@me.com>
2022-03-30 19:51:26 +0300

    Fix syntax

--- logzio.tf
+++ logzio.tf
@@ -6,10 +6,10 @@ resource aws_iam_role iam_lambda_cw_to_logzio {
     assume_role_policy = jsonencode({
         Version = "2012-10-17"
         Statement = [{
-            Action = "sts:AssumeRole"
-            Effect = "Allow"
-            Sid    = ""
-            Principal = {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Sid    = ""
+                Principal = {
                 Service = "lambda.amazonaws.com"
             }
         }]
